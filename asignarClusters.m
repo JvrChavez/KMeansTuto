06f1a67ae21c50dab40a2cc480da7929c6868619
JvrChavez <ariel.1999@outlook.com>
2024-03-19 03:30:59 -0700

    Asignacion de cluster
    Se asignan clusters a los datos pero aun no se actualizan los clusters conforme los datos

---
+++ asignarClusters.m
@@ -0,0 +1,22 @@
+%Funcion para asignar cluster
+function asignacionClusters = asignarClusters(datos, centroides)
+    num_puntos = size(datos, 1);
+    num_clusters = size(centroides, 1);
+    num_atributos = size(centroides, 2);
+    
+    % Inicializar matriz de asignación de puntos a clusters
+    asignacionClusters = zeros(num_puntos, 1);
+    
+    % Calculamos distancias
+    distancias = zeros(num_puntos, num_clusters);
+    for i = 1:num_clusters
+        for j = 1:num_puntos
+	    for o = 1:num_atributos
+    		distancias(j, i) = distancias(j, i) + ((datos(j, o) - centroides(i, o)).^2);
+	    end
+	end
+    end
+    
+    % Asignar cada punto al cluster más cercano
+    [~, asignacionClusters] = min(distancias, [], 2);
+end
---
+++ generarCentroides.m
@@ -0,0 +1,10 @@
+function centroides = generarCentroides(matriz,numCentroides,numCol)
+	centroides= zeros(numCentroides, numCol);
+	for i=1:numCentroides
+		for j=1:numCol
+		rango = max(matriz(:, j)) - min(matriz(:, j));
+		valor_aleatorio = min(matriz(:, j)) + rango * rand;
+		centroides(i, j) = valor_aleatorio;
+		end
+	end    
+end
